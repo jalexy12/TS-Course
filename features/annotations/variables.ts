a3e8d6dc4743e454024086261d337c0443f8de6f
Josh Alexy <jalexy12@gmail.com>
2020-09-24 16:04:08 -0500

    Variable type annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -8,7 +8,6 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 // Array
-
 let colors: string[] = ['red', 'green', 'blue'];
 let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
@@ -21,8 +20,41 @@ class Car {
 let car: Car = new Car();
 
 // Object Literal
-
 let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+// functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+logNumber(50);
+
+// Using Annotations
+
+// 1. function that returns Any type
+
+/*
+  JSON.parse('false') -> false
+  JSON.parse(10) -> 10
+
+  JSON.parse returns any type because its too unpredictable
+*/
+const json = '{ "x": 10, "y": 20 }';
+const coordinates: { x: number, y: number } = JSON.parse(json);
+
+console.log(coordinates);
+
+
+// 2. Variables declared / initialized on two different lines
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
