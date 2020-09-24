ededb4f4f2070679e94e8e1d0e2486ba7f53033d
Josh Alexy <jalexy12@gmail.com>
2020-09-24 15:39:19 -0500

    More basics - arrays, classes, object literal types

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -5,5 +5,24 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 // built in objects
-
 let now: Date = new Date();
+
+// Array
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object Literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
