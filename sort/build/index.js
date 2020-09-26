a59527a5a8d4063b6237862f3ac9a8597b236f67
Josh Alexy <jalexy12@gmail.com>
2020-09-26 11:41:51 -0500

    setup basic TSC automation

---
+++ sort/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('hi there');
+function logSomething() {
+    console.log('something');
+}
+console.log("what");
---
+++ sort/src/index.ts
@@ -0,0 +1,7 @@
+console.log('hi there');
+
+function logSomething() {
+  console.log('something');
+}
+
+console.log("what");
